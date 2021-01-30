0050f6ebd75fa20eb5e21ba12d8a3a2ab1818aa0
Madd0c101 <newdagon@mail.ru>
2021-01-30 17:23:18 +0200

    0-Creation

---
+++ src/main/java/Greetings.java
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 30.01.2021.
+ */
+public class Greetings {
+    public static void main(String... args) {
+        System.out.println("First git initiation project");//fir
+        System.out.println("Second line");//sec
+        WhoAmI w=new WhoAmI();
+        w.name="George";
+        w.sayName();
+    }
---
+++ src/main/java/WhoAmI.java
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 30.01.2021.
+ */
+public class WhoAmI {
+    public String name;
+    public WhoAmI(String name) {
+        this.name = name;
+    }
+    public WhoAmI() {
+
+    }
+    public void sayName()
+    {
+        System.out.println(name);
+    }
+}
